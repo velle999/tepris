73446d615b5ae8a07d91ed6cb199f9293a55c7af
Velle999 <brncomputerhelp@gmail.com>
2026-02-14 12:39:22 -0600

    0.1.0

--- main.js
+++ main.js
@@ -5,14 +5,50 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
-    fullscreen: true,
+    minWidth: 800,
+    minHeight: 600,
+    icon: path.join(__dirname, 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
-      nodeIntegration: false
-    }
+      nodeIntegration: false,
+      sandbox: false
+    },
+    resizable: true,
+    fullscreen: false,
+    autoHideMenuBar: true,
+    backgroundColor: '#000000'
   });
-win.loadFile(path.join(__dirname, 'app', 'index.html'));
+
+  // Load the index.html file
+  win.loadFile('index.html');
+  
+  // Open DevTools in development mode
+  if (!app.isPackaged) {
+    win.webContents.openDevTools();
+  }
+
+  // Handle window closing
+  win.on('closed', () => {
+    // Dereference the window object
+  });
 }
 
-app.whenReady().then(createWindow);
+// This method will be called when Electron has finished initialization
+app.whenReady().then(() => {
+  createWindow();
+
+  // On macOS it's common to re-create a window in the app when the dock icon is clicked
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
